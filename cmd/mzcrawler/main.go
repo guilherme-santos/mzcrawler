b26a67a4e14326b5b44f37a287ea5ee468813793
Guilherme Santos <guilherme.santos@foodora.com>
2018-12-27 13:53:26 +0000

    Add main file

---
+++ cmd/mzcrawler/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/guilherme-santos/mzcrawler/http"
+)
+
+var followSubDomains = flag.Bool("subdomains", false, "sets if should follow subdomains")
+
+func main() {
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s <url>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	baseurl := flag.Arg(0)
+
+	c, err := http.NewWebCrawler(baseurl)
+	if err != nil {
+		fmt.Printf("Unable to create a crawler to %s: %s\n", baseurl, err)
+		os.Exit(1)
+	}
+	c.FollowSubDomains = *followSubDomains
+
+	sitemap, err := c.Crawl()
+	if err != nil {
+		fmt.Printf("Unable to crawl %s: %s\n", baseurl, err)
+		os.Exit(1)
+	}
+
+	// Print sitemap returned.
+	j, _ := json.MarshalIndent(sitemap, "", "   ")
+	fmt.Println(string(j))
+}
